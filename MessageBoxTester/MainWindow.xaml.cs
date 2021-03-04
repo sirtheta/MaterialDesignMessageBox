0254fe289c0bf9034fa944fd68f55b574be9da78
sirtheta <neo38@bluewin.ch>
2021-03-04 09:31:14 +0100

    added a messagebox tester

---
+++ MessageBoxTester/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using MaterialDesignMessageBoxSirTheta;
+
+namespace MessageBoxTester
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    public static bool ShowMessageBox(string messageStr, MessageType type, MessageButtons buttons)
+    {
+      return (bool)new MaterialDesignMessageBox(messageStr, type, buttons).ShowDialog();
+    }
+
+    private void ButtonOk_Click(object sender, RoutedEventArgs e)
+    {
+      if (ShowMessageBox("Button ok cklicked", MessageType.Info, MessageButtons.Ok))
+      {
+        txtBox.Text = "true";
+      }
+    }
+
+    private void ButtonOkCancel_Click(object sender, RoutedEventArgs e)
+    {
+      if (ShowMessageBox("Button Yes/No cklicked", MessageType.Confirmation, MessageButtons.YesNo))
+      {
+        txtBox.Text = "true";
+      }
+      else
+      {
+        txtBox.Text = "false";
+      }
+    }
+
+    private void ButtonYesNo_Click(object sender, RoutedEventArgs e)
+    {
+      if(ShowMessageBox("Button ok/cancel Cklicked", MessageType.Warning, MessageButtons.OkCancel))
+      {
+        txtBox.Text = "true";
+      }
+      else
+      {
+        txtBox.Text = "false";
+      }
+    }
+  }
+}
