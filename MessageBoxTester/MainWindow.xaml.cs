24ce3b327a0f8216fc875ebecd376cd6e821f92c
sirtheta <sirtheta@gmail.com>
2024-07-26 17:55:46 +0200

    fixed bug with new guidlines for brushes

--- MessageBoxTester/MainWindow.xaml.cs
+++ MessageBoxTester/MainWindow.xaml.cs
@@ -1,51 +1,51 @@
-﻿using System.Windows;
-using MaterialDesignMessageBoxSirTheta;
+﻿using MaterialDesignMessageBoxSirTheta;
 using MaterialDesignMessageBoxSirTheta.Definitions;
+using System.Windows;
 
 namespace MessageBoxTester
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void ButtonOk_Click(object sender, RoutedEventArgs e)
-        {
-            txtBox.Text = MaterialDesignMessageBox.Show("Button ok clicked", MessageType.Info, MessageButtons.Ok).ToString();
-        }
-
-        private void ButtonOkCancel_Click(object sender, RoutedEventArgs e)
-        {
-            txtBox.Text = MaterialDesignMessageBox.Show("Button Yes/No clicked", MessageType.Confirmation, MessageButtons.YesNo).ToString();
-        }
-
-        private void ButtonYesNo_Click(object sender, RoutedEventArgs e)
-        {
-
-            txtBox.Text = MaterialDesignMessageBox.Show("Button ok/cancel clicked", MessageType.Warning, MessageButtons.OkCancel).ToString();
-        }
-
-        private void ButtonSuccess_Click(object sender, RoutedEventArgs e)
-        {
-
-            txtBox.Text = MaterialDesignMessageBox.Show("Button Success clicked", MessageType.Success, MessageButtons.Ok).ToString();
-
-        }
-        private void ButtonError_Click(object sender, RoutedEventArgs e)
-        {
-
-            txtBox.Text = MaterialDesignMessageBox.Show("Button Error clicked", MessageType.Error, MessageButtons.Ok).ToString();
-
-        }
-
-        private void ButtonCustom_Click(object sender, RoutedEventArgs e)
-        {
-            txtBox.Text = MaterialDesignMessageBox.Show("Button custom clicked", MessageType.Question, MessageButtons.Custom, "do that little thing inside your head and spin around", "do this little thing inside your head and spin around").ToString();
-        }
+      InitializeComponent();
     }
+
+    private void ButtonOk_Click(object sender, RoutedEventArgs e)
+    {
+      txtBox.Text = MaterialDesignMessageBox.Show("Button ok clicked", MessageType.Info, MessageButtons.Ok).ToString();
+    }
+
+    private void ButtonOkCancel_Click(object sender, RoutedEventArgs e)
+    {
+      txtBox.Text = MaterialDesignMessageBox.Show("Button Yes/No clicked", MessageType.Confirmation, MessageButtons.YesNo).ToString();
+    }
+
+    private void ButtonYesNo_Click(object sender, RoutedEventArgs e)
+    {
+
+      txtBox.Text = MaterialDesignMessageBox.Show("Button ok/cancel clicked", MessageType.Warning, MessageButtons.OkCancel).ToString();
+    }
+
+    private void ButtonSuccess_Click(object sender, RoutedEventArgs e)
+    {
+
+      txtBox.Text = MaterialDesignMessageBox.Show("Button Success clicked", MessageType.Success, MessageButtons.Ok).ToString();
+
+    }
+    private void ButtonError_Click(object sender, RoutedEventArgs e)
+    {
+
+      txtBox.Text = MaterialDesignMessageBox.Show("Button Error clicked", MessageType.Error, MessageButtons.Ok).ToString();
+
+    }
+
+    private void ButtonCustom_Click(object sender, RoutedEventArgs e)
+    {
+      txtBox.Text = MaterialDesignMessageBox.Show("Button custom clicked", MessageType.Question, MessageButtons.Custom, "do that little thing inside your head and spin around", "do this little thing inside your head and spin around").ToString();
+    }
+  }
 }
